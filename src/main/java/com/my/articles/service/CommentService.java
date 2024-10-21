1479b73d39f864471e02f462eeaf3bb99fa5b660
yeanhee129 <godqhr12392@gmail.com>
2024-10-21 12:13:29 +0900

    Articles 예외처리, Comment API 작업

--- src/main/java/com/my/articles/service/CommentService.java
+++ src/main/java/com/my/articles/service/CommentService.java
@@ -5,6 +5,7 @@ import com.my.articles.dto.CommentDTO;
 import com.my.articles.entity.Comment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,16 +21,21 @@ public class CommentService {
     }
 
     public void insertComment(Long articleId, CommentDTO dto) {
-        commentDAO.insertComment(articleId,
-                CommentDTO.fromDto(dto));
+        commentDAO.insertComment(articleId, CommentDTO.fromDto(dto));
     }
 
     public Map<String, Object> findByIdComment(Long commentId) {
         Comment comment = commentDAO.findByIdComment(commentId);
         Map<String, Object> map = new HashMap<>();
-        map.put("dto", CommentDTO.fromEntity(comment));
-        map.put("articleId", comment.getArticle().getId());
-        return map;
+        if(ObjectUtils.isEmpty(comment)){
+            map.put("dto", null);
+            map.put("articleId", null);
+            return map;
+        }else{
+            map.put("dto", CommentDTO.fromEntity(comment));
+            map.put("articleId", comment.getArticle().getId());
+            return map;
+        }
     }
 
     public void updateComment(CommentDTO dto) {
